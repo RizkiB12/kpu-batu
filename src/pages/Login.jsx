57f0600028a0c76136599d962b12b41d14fabe4d
RizkiB12 <rbagus306@gmail.com>
2022-12-02 15:07:57 +0700

    remove remember me in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -62,12 +62,6 @@ const Login = () => {
                             />
                         </Form.Item>
                         <Form.Item>
-                            <Form.Item name="remember" valuePropName="checked" noStyle>
-                                <Checkbox>Remember me</Checkbox>
-                            </Form.Item>
-                        </Form.Item>
-
-                        <Form.Item>
 
                             <Button type="primary" htmlType="submit" className="login-form-button">
                                 Log in
